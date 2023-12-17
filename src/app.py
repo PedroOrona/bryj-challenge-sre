69e559db9bdb8f62b76fabc92d51588887bd514e
PedroOrona <pedroaugusto.eng@gmail.com>
2023-12-17 16:33:09 +0000

    Collect the metrics and check if alarm needs to be triggered

--- src/app.py
+++ src/app.py
@@ -4,9 +4,9 @@ metric from the cAdvisor stats field (you can do this by checking json structure
 cAdivsor returns). You should check the json key names needed for this metric to get to the
 actual value that will be used for checking the threshold.
 """
-# import os, requests
-
 import concurrent.futures
+import os
+import time
 
 import json
 import logging
@@ -14,27 +14,57 @@ from urllib.request import urlopen
 
 import boto3
 
+VALUES_FILE_NAME = "metric_values.json"
 CONTAINER_NAME = "redis"
-DEFAULT_THRESHOLD = 3
-DEFAULT_WINDOW = 5
+
+DEFAULT_WINDOW = 4
+MINUTE_PERIOD = 2
+
+logging.basicConfig(level=logging.INFO)
+
+
+def alarm_action(metric_name: str, upload: bool = False):
+    """Execute an specific action depending of the alarm triggered"""
+    logging.info("Alarm triggered for metric %s", metric_name)
 
 
 def check_value(container_info: dict, metric: dict):
     """TO-DO"""
-    logging.info("Collecting metric %s/%s", metric.name, metric.key)
+    metric_name = metric.get("name")
+    metric_threshold = metric.get("threshold")
+    logging.info("Collecting metric %s", metric_name)
     metric_info = {}
     try:
-        info = container_info.values()
+        info = list(container_info.values())[0]
         if CONTAINER_NAME in info.get("aliases"):
-            stats = info.get("stats")
+            # Always get the most recent metrics status
+            stats = info.get("stats")[-1]
+            metric_value = stats.get(metric.get("area"))
 
-            metric_value = stats.get(metric.name)
             # Not elegant (probably there's a better way of doing it)
-            for key in metric.keys:
+            for key in metric.get("keys"):
                 metric_value = metric_value.get(key)
 
+            metric_info["name"] = metric_name
             metric_info["value"] = metric_value
+            metric_info["alarm"] = False
+            metric_info["window"] = metric.get("window", DEFAULT_WINDOW)
             metric_info["timestamp"] = stats.get("timestamp")
+
+            if metric_value >= metric_threshold:
+                metric_info["alarm"] = True
+                logging.info(
+                    "Metric value over threshold (%s) for metric %s",
+                    metric_threshold,
+                    metric_name,
+                )
+
+            logging.info(
+                "Metric collected! %s: %s (%s)",
+                metric_info["name"],
+                metric_info["value"],
+                metric_info["timestamp"],
+            )
         else:
             logging.info(
                 "cAdvisor doesn't have information for %s container", CONTAINER_NAME
@@ -48,19 +78,51 @@ def check_value(container_info: dict, metric: dict):
         return None
 
 
-def collect_metrics(container_info: dict, metrics: dict):
+def collect_metrics(container_info: dict, metrics: list[dict], alarm: dict):
     """
     For each metric defined in the metrics.json, collect its values
     from the cAdvisor collected container information
     """
-    with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
+    # TO-DO: Make a condition to not allow a very big number of metrics
+    with concurrent.futures.ProcessPoolExecutor(max_workers=len(metrics)) as executor:
         results = [
             executor.submit(check_value, container_info, metric) for metric in metrics
         ]
 
         for future in concurrent.futures.as_completed(results):
             result = future.result()
-            print(result)
+            # Make the result have only the metric value and timestamp to save to the json file
+            metric_name = result.pop("name")
+
+            if result.get("alarm") and not alarm.get(metric_name):
+                alarm[metric_name] = {"status": result.get("alarm"), "period": 0}
+            elif result.get("alarm") and alarm[metric_name]["status"]:
+                alarm[metric_name]["period"] += MINUTE_PERIOD
+            elif result.get("alarm"):
+                alarm[metric_name] = {"status": True, "period": MINUTE_PERIOD}
+            else:
+                alarm[metric_name] = {"status": False, "period": 0}
+
+            if alarm[metric_name].get("period") >= result.get("window"):
+                alarm_action(metric_name)
+
+            if os.path.exists(VALUES_FILE_NAME):
+                with open(VALUES_FILE_NAME, "r", encoding="utf-8") as f:
+                    metric_values = json.load(f)
+
+                metric_values[metric_name].append(result)
+            else:
+                logging.info(
+                    "Creating new JSON file called %s for saving metric values.",
+                    VALUES_FILE_NAME,
+                )
+                metric_values = {metric_name: [result]}
+
+            # overwrite/create file
+            with open(VALUES_FILE_NAME, "w", encoding="utf-8") as f:
+                json.dump(metric_values, f)
+
+    return alarm
 
 
 def main():
@@ -71,10 +133,14 @@ def main():
     response = urlopen(parsing_url)
     container_info = json.loads(response.read())
 
-    with open("metrics.json", encoding="utf-8") as f:
-        metrics = f.read()
+    with open("src/metrics.json", encoding="utf-8") as f:
+        metrics = json.load(f)
+    metrics = metrics.get("metrics")
 
-    collect_metrics(container_info, metrics)
+    alarm = {}
+    while True:
+        alarm = collect_metrics(container_info, metrics, alarm)
+        time.sleep(MINUTE_PERIOD * 60)
 
 
 if __name__ == "__main__":
